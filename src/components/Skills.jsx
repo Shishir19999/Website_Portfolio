ace7a783001cfcc8eb204c9bf9e2ea257911f22e
Shishir19999 <shishirsharma199@gmail.com>
2024-05-26 21:41:39 +0545

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,25 +4,22 @@ import skills from "./data/skills.json";
 const Skills = () => {
   return (
     <>
-    <div className="container">
-    <h1>SKILLS</h1>
-    </div>
-    <br />
+      <div className="container">
+        <h1>SKILLS</h1>
+      </div>
+      <br />
       <div className="container skills" id="skills">
-        
-        <div className="items ">
-          {skills.map((data) => (
-            <>
-              <div
-                className="item"
-                key={data.id}
-                data-aos="flip-left"
-                data-aos-duration="1000"
-              >
-                <img src={`/assets/${data.imageSrc}`} alt="" />
-                <h3>{data.title}</h3>
-              </div>
-            </>
+        <div className="items">
+          {skills.map((data, index) => (
+            <div
+              className="item"
+              key={index} // Using index as the key
+              data-aos="flip-left"
+              data-aos-duration="1000"
+            >
+              <img src={`${data.imageSrc}`} alt="" />
+              <h3>{data.title}</h3>
+            </div>
           ))}
         </div>
       </div>
